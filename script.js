a018fc8febbc0f0beafc237c8d853b013913c419
ondjoss <ghislain.chakam@gmail.com>
2022-07-16 22:37:50 +0200

    Ajout de la zone de circulation de l'acteur...

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+
+    let canvas = document.createElement("canvas");
+    canvas.width = 900;
+    canvas.height = 600;
+    canvas.style.border = "1px solid black";
+    document.body.appendChild(canvas);
+}
